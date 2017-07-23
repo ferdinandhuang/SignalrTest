e3f79df7897c4cf0298d61e4e0c4f8a7bcdf8e46
Ferdinand <FerdinandHuang@hotmail.com>
2017-07-23 15:43:12 +0800

    新建项目

---
+++ SignalrTest/App_Code/MyConnection.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SignalrTest.App_Code
+{
+    public class MyConnection : PersistentConnection
+    {
+        private static readonly ConcurrentDictionary<string, string> _users = new ConcurrentDictionary<string, string>();
+        private static readonly ConcurrentDictionary<string, string> _clients = new ConcurrentDictionary<string, string>();
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            Cookie userNameCookie;
+            if (request.Cookies.TryGetValue("user", out userNameCookie) &&
+                userNameCookie != null)
+            {
+                _clients[connectionId] = userNameCookie.Value;
+                _users[userNameCookie.Value] = connectionId;
+            }
+
+            string clientIp = GetClientIP(request);
+
+            string user = GetUser(connectionId);
+
+            return Connection.Send(connectionId, "Welcome!");
+        }
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Send(connectionId, data);
+        }
+
+        protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
+        {
+            Debug.WriteLine("OnDisconnected");
+            return base.OnDisconnected(request, connectionId, stopCalled);
+        }
+
+        protected override Task OnReconnected(IRequest request, string connectionId)
+        {
+            Debug.WriteLine("OnReconnected");
+            return base.OnReconnected(request, connectionId);
+        }
+
+        /// <summary>
+        /// 通过connectionId获取用户名
+        /// </summary>
+        /// <param name="connectionId"></param>
+        /// <returns></returns>
+        private string GetUser(string connectionId)
+        {
+            string user;
+            if (!_clients.TryGetValue(connectionId, out user))
+            {
+                return connectionId;
+            }
+            return user;
+        }
+
+        /// <summary>
+        /// 通过user获取connectionId
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private string GetClient(string user)
+        {
+            string connectionId;
+            if (_users.TryGetValue(user, out connectionId))
+            {
+                return connectionId;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 连接类型
+        /// </summary>
+        enum MessageType
+        {
+            Send,
+            Broadcast,
+            Join,
+            PrivateMessage,
+            AddToGroup,
+            RemoveFromGroup,
+            SendToGroup,
+            BroadcastExceptMe,
+        }
+        class Message
+        {
+            public MessageType Type { get; set; }
+            public string Value { get; set; }
+        }
+
+        private static string GetClientIP(IRequest request)
+        {
+            return Get<string>(request.Environment, "server.RemoteIpAddress");
+        }
+
+        private static T Get<T>(IDictionary<string, object> env, string key)
+        {
+            object value;
+            return env.TryGetValue(key, out value) ? (T)value : default(T);
+        }
+    }
+}
---
+++ SignalrTest/App_Code/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Test2.App_Code;
+
+[assembly: OwinStartup(typeof(SignalrTest.App_Code.Startup))]
+
+namespace SignalrTest.App_Code
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR<MyConnection>("/my-connection");
+        }
+    }
+}
